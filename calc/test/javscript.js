cad909cb1a73d2bd9d5a22fbce125fbddb954f10
Adryan r <forget.f255@gmail.com>
2023-06-10 00:43:07 -0300

    Adiciona projeto calculadora (calc)
    Adiciona projeto html, css e javascript de uma calculadora

---
+++ calc/test/javscript.js
@@ -0,0 +1,8 @@
+// window.resizeTo(450, 450);
+
+// window.resizeby(100, 100);
+// window.resizeBy(100, -50);
+function quarter() {
+    window.resizeTo(window.screen.availWidth / 2, window.screen.availHeight / 2);
+  }
+quarter()
