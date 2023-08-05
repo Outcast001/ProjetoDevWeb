48865adcd75bdeca40cbcfc8d972ec4d38f91b41
Adryan r <forget.f255@gmail.com>
2023-08-05 19:38:34 -0300

    Substitui "Todo" por "To-do"

--- calc/test/javascript.js
+++
@@ -1,9 +0,0 @@
-var Id = 12+1;
-var str = `codigo: ${Id}`;
-console.log(str);
-console.log("Test...");
-var n1 = 1;
-var operator = '+';
-var n2 = 1;
-var result = parseInt(n1) + parseInt(n2);
-console.log(parseInt(result));
